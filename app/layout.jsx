dc9dcf304be63712e5b288fc25884970932920a3
Michalis Meliopoulos <mixalismeliop@gmail.com>
2023-05-16 18:02:17 +0300

    logo change

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,12 +6,15 @@ import Provider from "@components/Provider";
 export const metadata = {
   title: "Promptopia",
   description: "Discover and share AI Prompts",
+  icons: {
+    icon: "/assets/images/final-logo-round2-small.png",
+  },
 };
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <Provider>
           <div className="main">
             <div className="gradient"></div>
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -22,10 +22,10 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src="/assets/images/logo.svg"
+          src="/assets/images/final-logo-round2-small.png"
           alt="logo"
-          width={30}
-          height={30}
+          width={50}
+          height={50}
           className="object-contain"
         />
         <p className="logo_text">Promptopia</p>
